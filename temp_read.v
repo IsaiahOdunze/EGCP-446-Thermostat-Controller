fb915d0b9435a513ccecb734aee9caad0f8bb66d
ANTHONY RUIZ <46951916+anthony7586@users.noreply.github.com>
2022-12-01 21:30:33 -0800

    Update temp_read.v

--- temp_read.v
+++ temp_read.v
@@ -1,69 +1,36 @@
 `timescale 1ns / 1ps
-/*
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
+//////////////////////////////////////////////////////////////////////////////////
+// Company: CSUF
 // Engineer: Anthony Ruiz
-// Create Date: 11/29/2022 10:29:36 AM 
-// Module Name: temp_read
-// Project Name: term project temp_read module 
-// Target Devices: nexys 4ddr 
- 
-// Description: 
-            this module takes in 4 inputs and has one output 
-            the output is the tempeture reading in ferenheit as "temp_out"
-            temp_out is a bus sinc it is an intiger of decimal radix
-*/
-////////////////////////////////////////////////////////////////////////////////
+// 
+// Create Date: 11/09/2014 03:46:42 PM
+// Module Name: temp
+// Project Name: temp project 
+// Description: manually inputs data using switches 15,14,13,12
+//////////////////////////////////////////////////////////////////////////////////
 
-/* ///////////////////////////////////////////////////////////////////////////////
-                             temperature read module
-/////////////////////////////////////////////////////////////////////////////// */
-module temp_read(
-    input clk,
-    input reset,
-    input [12:0] temp_in,
-    output [12:0] temp_out // Data width is 13 for the ADT7420 Temperature Sensor
-    );
-    //more infor on the temp sensor 
-   // https://users.wpi.edu/~rjduck/Temp%20Sensor%20Data%20Sheets.pdf
-   // https://stackoverflow.com/questions/36946405/adt7420-temperature-reading-verilog
-   
-    wire slowclock;
-    
-    assign temp_out = 1.8*(temp_in - 273) + 32; //converts from kelvin to fahrenheit
-    
-    
-    clock_divider clkDiv1(.clk(clk),.reset(reset),.sclk(slowclock));
-    temp_read tr1 (.clk(slowkclock),.reset(reset),.temp_in(temp_in),.temp_out(temp_out));
-    
-    
-    
-endmodule
 
-/* ///////////////////////////////////////////////////////////////////////////////
-                       clock divider/slow clock module
-/////////////////////////////////////////////////////////////////////////////// */
+module input_temp(
+    input clk, //i dont think you need a clock since we are hard setting temperature using switches
+	input [3:0] switches, //each one has a set etmperature to it 
+	output reg [1:0]temp_out
 
-module clock_divider(
- 
-input clk,
-input reset,
-output reg sclk
-);
+	
+	);
 
-reg [31:0] count;
-    always@(posedge clk or posedge reset)
-        begin
-    if(reset == 1'b1) begin
-    count <= 32'd0;
-    sclk <= 1'b0;
-        end else begin
-    if(count == 32'd60000000) begin 
-    count <= 32'd0;
-    sclk <= ~sclk;
-        end else begin
-    count <= count + 1;
-        end
-    end
-    end
-    endmodule
+   always @ (switches)begin 
+    case(switches)
+        //binary ---------> decimal
+        4'b0000: temp_out = 90;
+        4'b1001: temp_out = 85;
+        4'b0100: temp_out = 75;
+        4'b0010: temp_out = 60;
+        4'b0001: temp_out = 46;
+
+        default: temp_out = 75; 
+        
+    endcase 
+    end 
+
+
+endmodule 
