426e9be59946db1b2c7bd51da2274bf58ad5125e
Connor Bartholomew <43282155+cbartholomew01@users.noreply.github.com>
2022-12-02 21:38:26 -0800

    Updated comments

--- sseg_display.v
+++ sseg_display.v
@@ -1,25 +1,33 @@
 `timescale 1ns / 1ps
-// Created by David J. Marion
-// Date 7.19.2022
-// 7 Segment Control for the Nexys A7 Temperature Sensor
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Engineer: Connor Bartholomew
+// Create Date: 12/2/2022 7:35:55 PM
+// Module Name: sseg_display
+// Project Name: EGCP446_Thermostat
+// Description: This module has 3 inputs and 2 outputs. It takes in a current temperature
+//              and a changed temperature and displays them across the separate anodes of
+//              the seven segment display.
+/
+//////////////////////////////////////////////////////////////////////////////////
 
 module sseg_display(
-    input clk,               // Nexys A7 clock
-    input [7:0] CurrentTemp,          // Temp data from i2c master
+    input clk,
+    input [7:0] CurrentTemp,
     input [7:0] ChangedTemp,
-    output reg [6:0] sseg_out,           // 7 Segments of Displays
-    output reg [7:0] an_out             // 4 Anodes of 8 to display Temp
+    output reg [6:0] sseg_out, //Segment of the seven segment display
+    output reg [7:0] an_out //Anodes of the seven segment display
     );
     
     wire [3:0] current_tens, current_ones;
-    assign current_tens = CurrentTemp / 10;           // Tens value of temp data
-    assign current_ones = CurrentTemp % 10;           // Ones value of temp data
+    assign current_tens = CurrentTemp / 10;           //Retrieves tens place of CurrentTemp
+    assign current_ones = CurrentTemp % 10;           //Retrieves ones place of CurrentTemp
     
     wire [3:0] changed_tens, changed_ones;
-    assign changed_tens = ChangedTemp / 10;           // Tens value of temp data
-    assign changed_ones = ChangedTemp % 10;           // Ones value of temp data
+    assign changed_tens = ChangedTemp / 10;           //Retrieves tens place of ChangedTemp
+    assign changed_ones = ChangedTemp % 10;           //Retreieves ones place of ChangedTemp
     
-    // Parameters for segment patterns
+    //Parameters for seven segment outputs
     parameter ZERO  = 7'b0000001;  // 0
     parameter ONE   = 7'b1001111;  // 1
     parameter TWO   = 7'b0010010;  // 2 
@@ -30,25 +38,24 @@ module sseg_display(
     parameter SEVEN = 7'b0001111;  // 7
     parameter EIGHT = 7'b0000000;  // 8
     parameter NINE  = 7'b0000100;  // 9
-    parameter DEG   = 7'b0011100;  // degrees symbol
-    parameter C     = 7'b0110001;  // 
+    parameter DEG   = 7'b0011100;  // degrees
+    parameter F     = 7'b0111000;  // Farenheit
     
-        // To select each digit in turn
-    reg [2:0] an_sel;         // 3 bit counter for selecting each of 8 digits
-    reg [16:0] counter;         // counter for digit refresh
+    reg [2:0] an_sel;   // 3 bit counter for selecting which anode to display to
+    reg [16:0] counter; // Counter for clock divider to refresh anodes
     
-        // Logic for controlling digit select and digit timer
+    //Clock Divider to handle refresh of anodes
     always @(posedge clk) begin
-        // 1ms x 8 displays = 8ms refresh period
-        if(counter == 49999) begin         // The period of 100MHz clock is 10ns (1/100,000,000 seconds)
-            counter <= 0;                   // 10ns x 100,000 = 1ms
+        if(counter == 49999) 
+        begin
+            counter <= 0;
             an_sel <=  an_sel + 1;
         end
         else
             counter <=  counter + 1;
     end
     
-        // Logic for driving the 4 bit anode output based on digit select
+    //Case statement to select which anode to display to based on an_sel from clock divider
     always @(an_sel) begin
         case(an_sel) 
             3'b000 : an_out = 8'b11111110;   // Turn on ones place of first set
@@ -64,12 +71,12 @@ module sseg_display(
     
         always @*
         case(an_sel)
-            3'b000 : sseg_out = C;    // Set to C for Celsuis
+            3'b000 : sseg_out = F;    // Set to F for Farenheit
                         
-            3'b001 : sseg_out = DEG;  // Set to degrees symbol
+            3'b001 : sseg_out = DEG;  // Set to degrees
                     
-            3'b010 : begin       // TEMPERATURE ONES DIGIT
-                        case(current_ones)
+            3'b010 : begin
+                        case(current_ones) //CurrentTemp ones digit to display
                             4'b0000 : sseg_out = ZERO;
                             4'b0001 : sseg_out = ONE;
                             4'b0010 : sseg_out = TWO;
@@ -83,8 +90,8 @@ module sseg_display(
                         endcase
                     end
                     
-            3'b011 : begin       // TEMPERATURE TENS DIGIT
-                        case(current_tens)
+            3'b011 : begin
+                        case(current_tens) //CurrentTemp tens digit to display
                             4'b0000 : sseg_out = ZERO;
                             4'b0001 : sseg_out = ONE;
                             4'b0010 : sseg_out = TWO;
@@ -98,12 +105,12 @@ module sseg_display(
                         endcase
                     end
                     
-            3'b100 : sseg_out = C;    // Set to C for Celsuis
+            3'b100 : sseg_out = F;
                         
-            3'b101 : sseg_out = DEG;  // Set to degrees symbol
+            3'b101 : sseg_out = DEG;
             
             3'b110 : begin
-                        case(changed_ones)
+                        case(changed_ones) //ChangedTemp ones digit to display
                             4'b0000 : sseg_out = ZERO;
                             4'b0001 : sseg_out = ONE;
                             4'b0010 : sseg_out = TWO;
@@ -117,7 +124,7 @@ module sseg_display(
                         endcase
                     end
             3'b111 : begin
-                        case(changed_tens)
+                        case(changed_tens) //ChangedTemp tens digit to display
                             4'b0000 : sseg_out = ZERO;
                             4'b0001 : sseg_out = ONE;
                             4'b0010 : sseg_out = TWO;
